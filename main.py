b4189e84e5ffe2f37298a581501bcfad8f73049a
mr-bumblebee <kolyandr1993@mail.ru>
2021-06-07 15:50:53 +0500

    first app realisation(naive version)

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-import flask as fl
+from flask import Flask
 import sqlalchemy
+import sqlite3
 
-app = fl.Flask(__name__)
+import view
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello</h1>"
+
+
+@app.route("/user/<name>")
+def user(name):
+    return f"<h1>Hello {name}</h1>"
+
 
 if __name__ == '__main__':
-    pass
+    app.run(debug=True)
