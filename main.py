8e6996ed2b1cc0b4c748471d8d8cef7f465eb6c2
mr-bumblebee <kolyandr1993@mail.ru>
2021-06-09 12:08:03 +0500

    first app realisation-add templates, bootstrap

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-from flask import Flask, request, make_response, redirect, abort
+from flask import Flask, request, make_response, redirect, abort, render_template
+# from flask_script import Manager
+from flask_bootstrap import Bootstrap
 import sqlalchemy
 import sqlite3
 
 # секция комментариев
 # функция redirect() перенаправляет на другой сайт, принимает url
-#функция abort() обрабатывает ошибки,принимает код ошибки
+# функция abort() обрабатывает ошибки,принимает код ошибки
 import view
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
+# manager = Manager(app)
 
-# naive realisation handlers of pages
-
-@app.route("/")
-def index():
-    user_agent = request.headers.get("User-Agent")
-    print(app.url_map)
-
-    return f"<h1>Hello {user_agent}</h1>"
+@app.route("/<name>")
+def index(name):
+    return render_template("index.html", name=name)
 
 
 @app.route("/user/<name>")
@@ -25,12 +24,22 @@ def user(name):
     if name == "Nik":
         abort(400)
     else:
-        return f"<h1>Hello {name}</h1>"
+        return render_template("user.html", name=name)
 
 
 @app.route("/bad/")
 def bad():
     return redirect("http://ya.ru")
+
+
+@app.errorhandler(404)
+def page_not_found(err):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def enternal_server_error(err):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
