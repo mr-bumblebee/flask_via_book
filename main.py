9f5e49c8d369d1ad08e371ddd372d2a34c462dd3
mr-bumblebee <kolyandr1993@mail.ru>
2021-06-10 18:44:30 +0500

    first app realisation-add flash messages->changed main.py and base.html

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, make_response, redirect, abort, render_template, session, url_for
+from flask import Flask, request, make_response, redirect, abort, render_template, session, \
+    url_for, flash
 # from flask_script import Manager
 from flask_bootstrap import Bootstrap
 #library for work with local date/time
@@ -30,12 +31,16 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session["name"]
+        if old_name is not None and old_name != form.name.data:
+            flash("You changed name")
         session["name"] = form.name.data
+        form.name.data = ''
         return redirect(url_for("index"))
     return render_template("index.html",
                            current_time=datetime.utcnow(),
                            form=form,
-                           name=session["name"]) # add redirecting with saveing  entering name
+                           name=session["name"])
 
 
 @app.route("/user/<name>")
